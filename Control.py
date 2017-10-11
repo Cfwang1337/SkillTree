fba08038a915cf8c35d6569c4ca653f4e9694192
Charles Wang <Cfwang1337@gmail.com>
2017-10-10 22:22:33 -0700

    Visualization works but need to consider pruning and categorization by neighborliness

--- Control.py
+++ Control.py
@@ -1,11 +1,86 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
+from collections import defaultdict
 import csv
+import matplotlib.pyplot as plt
+import networkx as net
 import requests
 
 
-#TODO MIGHT NEED TO USE IMAP
+def trim_nodes(g, degree=1):
+    with open("subject_and_connection.csv", "wb") as writefile:
+        fieldnames = ["subject", "connections"]
+        writer = csv.DictWriter(writefile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        g2 = g.copy()
+        d = net.degree(g2)
+        for n in g.nodes():
+            writer.writerow(dict(
+                subject=n,
+                connections=d[n]
+            ))
+            # print n, d[n]
+            if d[n] <= degree:
+                g2.remove_node(n)
+        return g2
+
+
+def trim_edges(g, weight):
+    g2 = net.DiGraph()
+    for f, to, edata in g.edges(data=True):
+        # print f, to, edata
+        if edata['followers'] >= weight:
+            g2.add_edge(f, to, followers=edata['followers'])
+    return g2
+
+
+def analyze_follows():
+
+    o = net.DiGraph()
+
+    with open("follower_list.csv", "rb") as readfile:
+        reader = csv.DictReader(readfile)
+        for row in reader:
+            o.add_edge(row['Source'], row['Target'], followers=row['followers'])
+
+    print 'Graph size: ', len(o)
+    o = trim_nodes(o, degree=1)
+    print "After node pruning: ", len(o)
+    o = trim_edges(o, weight=1)
+    print "After edge pruning: ", len(o)
+
+    pos = net.spring_layout(o)
+
+    plt.axis('off')
+
+    ns_list = []
+
+    for node in o.nodes:
+        if len(list(o[node])) > 0:
+            nodesize = int(o[node][list(o[node])[0]]['followers'])
+        else:
+            nodesize = 1
+        ns_list.append(nodesize)
+
+        x, y = pos[node]
+
+        plt.text(x, y, s=node, alpha=1.0, horizontalalignment='center', fontsize=5)
+
+    net.draw_networkx_nodes(o, pos, node_color='g', node_size=ns_list, alpha=0.5)
+    net.draw_networkx_edges(o, pos, width=0.1, alpha=0.15)
+
+    plt.show()
+
+
+    #TODO NEED TO IDENTIFY DIFFERENT CLUSTERS
+    #TODO NEED DIFFERENT COLORS FOR DIFFERENT CLUSTERS
+
+    return
+
+
+#TODO NEED TO USE IMAP
 def followed_by_getter():
     prefix = "https://en.wikipedia.org/w/index.php?title=Special%3AWhatLinksHere&limit=500&target="
     suffix = "&namespace=0"
@@ -13,8 +88,9 @@ def followed_by_getter():
     with open("master_discipline_list.csv", 'rb') as readfile, open("follower_list.csv", "wb") as writefile:
 
         fieldnames = [
-            "subject",
-            "follower"
+            "Source",
+            "Target",
+            "followers"
         ]
         writer = csv.DictWriter(writefile, fieldnames=fieldnames)
         writer.writeheader()
@@ -30,7 +106,7 @@ def followed_by_getter():
             followers_subsetted = []
 
             while next_page is True:
-                print address
+                # print address
                 page = requests.get(address, timeout=30)
                 souper = BeautifulSoup(page.content, "html.parser")
 
@@ -40,24 +116,30 @@ def followed_by_getter():
                     break
 
                 for follower in followers:
-                    follower_prospect = follower.a.attrs.get('href').replace('/wiki/','').lower()
+                    follower_prospect = follower.a.attrs.get('href').replace('/wiki/', '').lower()
                     if follower_prospect in subjects:
                         followers_subsetted.append(follower_prospect)
 
+                #TODO THIS IS WRONG
                 next_index = souper.find('hr').find_next('a')
+                next_next_index = next_index.find_next('a')
 
                 if "next 500" in next_index.getText():
                     next_page = True
                     address = "http://en.wikipedia.org{0}".format(next_index.attrs.get('href'))
+                elif "next 500" in next_next_index.getText():
+                    next_page = True
+                    address = "http://en.wikipedia.org{0}".format(next_next_index.attrs.get('href'))
                 else:
                     next_page = False
 
-            print "FOLLOWER SUBSET:", len(followers_subsetted)
+            # print "FOLLOWER SUBSET:", len(followers_subsetted)
 
             for sub_follower in followers_subsetted:
                 writer.writerow(dict(
                     subject=subject,
-                    follower=sub_follower
+                    followed_by=sub_follower,
+                    followers=len(followers_subsetted)
                 ))
 
 
@@ -81,7 +163,6 @@ def wikipedia_getter():
         children = []
         for iteration in column_blocks:
 
-
             parent_1_link = [item.attrs.get('href').replace("/wiki/", "").lower() for item in iteration.find_previous('h2').find_next('div').find_all('a')\
                              if "Outline_" not in item.attrs.get('href')]
 
@@ -93,7 +174,7 @@ def wikipedia_getter():
                  if "Outline_" not in item.attrs.get('href') and "List" not in item.attrs.get('href')]
 
             for link in iteration.find_all('a'):
-                if "Outline_" not in link.attrs.get('href'):
+                if "Outline_" not in link.attrs.get('href') and "#" not in link.attrs.get('href'):
                     children.append(link.attrs.get('href').replace("/wiki/", "").lower())
             children.extend(parent_1_link)
             children.extend(parent_2_link)
@@ -111,16 +192,16 @@ def wikipedia_getter():
 def main():
 
     print "PLEASE CHOOSE"
-    print "0. SCRAPE WIKIPEDIA ACADEMICS MAIN PAGE "
-    print "1. SCRAPE WIKIPEDIA FOLLOWS "
-    # print "1. SCRAPE COURSE CATALOGS "
+    print "0. SCRAPE WIKIPEDIA ACADEMICS "
+    print "1. ANALYZE FOLLOWS GRAPH "
 
     choice = raw_input("ENTER CHOICE: ")
 
     if choice == "0":
         wikipedia_getter()
-    elif choice == "1":
         followed_by_getter()
+    elif choice == "1":
+        analyze_follows()
     return
 
 if __name__ == "__main__":
